e8d6702659f328a95281d5d8a6096a6cd68ca651
RuthlessJackal <89906905+RuthlessJackal@users.noreply.github.com>
2025-12-14 17:16:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from neopixel import NeoPixel
 from adc_sub import ADC_substitute
 from lmt87 import LMT87
 
-buzzer = PWM(Pin(14,Pin.IN), duty = 0)
+buzzer = PWM(Pin(32,Pin.IN), duty = 0)
 led1 = Pin(26,Pin.OUT)
 lcd = GpioLcd(rs_pin=Pin(27), enable_pin=Pin(25), d4_pin=Pin(33), d5_pin=Pin(32), d6_pin=Pin(21), d7_pin=Pin(22), num_lines=4, num_columns=20)
 threshhold = 3000
@@ -270,6 +270,7 @@ while True:
         print("Disconnected!")
         client.disconnect()
         exit()
+
 
 
 
