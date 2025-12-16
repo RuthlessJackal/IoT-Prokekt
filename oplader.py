f83356cd0d2edf5d012d626dcc4d5f9f99185955
RuthlessJackal <89906905+RuthlessJackal@users.noreply.github.com>
2025-12-16 21:51:08 +0100

    Implement CO2 emission monitoring and control
    Fetch CO2 emission data and control relay and LED based on the value.

---
+++ oplader.py
@@ -0,0 +1,22 @@
+import requests
+from time import sleep
+from machine import Pin
+
+relæ = Pin(14,Pin.OUT)
+led = Pin(26, Pin.OUT)
+
+while True:
+    response = requests.get(
+        url = 'https://api.energidataservice.dk/dataset/CO2Emis?limit=5')
+    result = response.json()
+    co2 = result['records'][1]['CO2Emission']
+    
+    if co2 <= 50:
+        relæ.on()
+        led.on()
+
+    elif co2 > 50:
+        relæ.off()
+        led.off()
+
+    sleep(5)
